cbceb71ed803772f06213c6f979fc72aab732741
Kshitiz Tiwari <kingkshitiz05@gmail.com>
2026-04-23 23:33:32 +0530

    deploy process

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.post(
 );
 
 app.use(cors(
-    {origin:"http://localhost:5173",
+    {origin: process.env.CLIENT_URL || "http://localhost:5173",
         credentials:true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
     }
